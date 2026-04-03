0b1ec60d386cef13aa5d8b3695f13a6ae03b94ea
GabiIsiani <isianigabrieli@gmail.com>
2026-04-03 19:04:54 +0400

    Initial commit - Spring Boot sports website

---
+++ src/main/java/com/sports/sportswebsite/SportswebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.sports.sportswebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportswebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportswebsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sports/sportswebsite/SportswebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sports.sportswebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportswebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
